3b688a6a7d68e8b2c45cf5c7f22b455c6fe238a7
GeorgiPavlov89 <georgi.pavlov89@gmail.com>
2022-06-14 10:39:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document from 'next/document'
+
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            (<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+           
+          </>
+        ),
+      }
+    } finally {
+      
+    }
+  }
+}
